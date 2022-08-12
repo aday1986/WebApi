7d3d826a594d3c6f2e8e81dda74ebc7cbf311328
link <aday@foxmail.com>
2022-08-12 16:22:48 +0800

    123

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -25,12 +25,13 @@ builder.WebHost
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
